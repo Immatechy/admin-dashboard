a09c9793320a52c55b3f6c42a06a13acde189df4
Immaculeta1 <immaculetapascaline@gmail.com>
2022-07-25 14:09:08 +0100

    Navbar created

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Topbar from "./components/topbar/Topbar";
+import Sidebar from "./components/sidebar/Sidebar"
+
+function App() {
+  return(
+    <div>
+      <Topbar/>
+      <div className="container">
+        <Sidebar />
+        other pages
+      </div>
+     
+    </div>
+  )
+
+}
+
+export default App;
