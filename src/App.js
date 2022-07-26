d48e0a83d15a621e1274d6dd1c040af9732a3396
Immaculeta1 <immaculetapascaline@gmail.com>
2022-07-26 10:04:15 +0100

    sidebar create

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Topbar from "./components/topbar/Topbar";
 import Sidebar from "./components/sidebar/Sidebar"
+import "./app.css";
 
 function App() {
   return(
@@ -7,7 +8,7 @@ function App() {
       <Topbar/>
       <div className="container">
         <Sidebar />
-        other pages
+        <div className="others">other pages</div>
       </div>
      
     </div>
