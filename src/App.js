30da4968580bb30039b04a97f533220651f68995
Immaculeta1 <immaculetapascaline@gmail.com>
2022-07-28 15:08:58 +0100

    Product List Create

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import  UserList  from "./pages/UserList/UserList"
 import User from "./pages/user/User";
 import NewUser from "./pages/newUser/NewUser";
+import ProductList from "./pages/productList/ProductList"
 
 function App() {
   return(
@@ -25,6 +26,9 @@ function App() {
           </Route>
           <Route path="/newUser">
             <NewUser />
+          </Route>
+          <Route path="/products">
+            <ProductList />
           </Route>
         </Switch>
       </div>
